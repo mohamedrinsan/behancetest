f7b684ec96bcbe531a2dea14d33515b6e4d017cd
mohamedrinsan <mohamedrinsan007@gmail.com>
2022-08-31 12:38:17 +0530

    home

--- post/models.py
+++ post/models.py
@@ -1,3 +1,14 @@
+from distutils.command import upload
+from email.mime import image
+from turtle import title
+from venv import create
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    cover = models.ImageField(upload_to="media", null = True, blank = True)
+    image1 = models.ImageField(upload_to="media", null = True, blank = True)
+    image2 = models.ImageField(upload_to="media", null = True, blank = True)
+
+    def __str__(self):
+        return self.title
